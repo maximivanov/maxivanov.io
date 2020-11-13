6136048192681f7c6280fc70a1b92d7edc7302e4
Max Ivanov <maximiv@gmail.com>
2020-11-13 02:23:57 +0000

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,138 @@
+const { DateTime } = require('luxon')
+const fs = require('fs')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(pluginSyntaxHighlight)
+
+  eleventyConfig.setDataDeepMerge(true)
+
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLL d, yyyy')
+  })
+
+  eleventyConfig.addFilter('monthAndDay', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLLL d')
+  })
+
+  eleventyConfig.addFilter('getYear', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy')
+  })
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd')
+  })
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
+      return array.slice(n)
+    }
+
+    return array.slice(0, n)
+  })
+
+  eleventyConfig.addFilter('min', (...numbers) => {
+    return Math.min.apply(null, numbers)
+  })
+
+  eleventyConfig.addCollection('tagList', function (collection) {
+    let tagSet = new Set()
+    collection.getAll().forEach(function (item) {
+      if ('tags' in item.data) {
+        let tags = item.data.tags
+
+        tags = tags.filter(function (item) {
+          switch (item) {
+            // this list should match the `filter` list in tags.njk
+            case 'all':
+            case 'post':
+            case 'posts':
+              return false
+          }
+
+          return true
+        })
+
+        for (const tag of tags) {
+          tagSet.add(tag)
+        }
+      }
+    })
+
+    // returning an array in addCollection works in Eleventy 0.5.3
+    return [...tagSet]
+  })
+
+  eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('android-chrome-192x192.png')
+  eleventyConfig.addPassthroughCopy('android-chrome-512x512.png')
+  eleventyConfig.addPassthroughCopy('apple-touch-icon.png')
+  eleventyConfig.addPassthroughCopy('browserconfig.xml')
+  eleventyConfig.addPassthroughCopy('favicon-16x16.png')
+  eleventyConfig.addPassthroughCopy('favicon-32x32.png')
+  eleventyConfig.addPassthroughCopy('favicon.ico')
+  eleventyConfig.addPassthroughCopy('mstile-150x150.png')
+  eleventyConfig.addPassthroughCopy('site.webmanifest')
+
+  /* Markdown Overrides */
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+  }).use(markdownItAnchor, {
+    permalink: true,
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#',
+  })
+  eleventyConfig.setLibrary('md', markdownLibrary)
+
+  // Browsersync Overrides
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html')
+
+        browserSync.addMiddleware('*', (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404)
+          res.end()
+        })
+      },
+    },
+    ui: false,
+    ghostMode: false,
+  })
+
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about those.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+    // pathPrefix: "/",
+
+    markdownTemplateEngine: 'liquid',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+
+    // These are all optional, defaults are shown:
+    dir: {
+      input: '.',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
+    },
+  }
+}
---
+++ posts/2012/redis-faina-whats-happening-to-my-redis-server.md
@@ -0,0 +1,36 @@
+---
+title: "redis-faina: what's happening to my Redis server?"
+description: This is literally the first blog post I published back in 2012.
+date: 2012-08-08
+tags:
+  - redis
+  - performance
+layout: layouts/post.njk
+---
+
+_Edit 2020: I'm not sure this is even relevant anymore, looks like project's Github is dead. This is here for history purposes._
+
+Following is about [redis-faina: a query analysis tool for Redis](http://instagram-engineering.tumblr.com/post/23132009381/redis-faina-a-query-analysis-tool-for-redis).
+
+It's a tool from the team at Instagram to analyze commands coming to your Redis server.
+
+You may have couple _(hundred (thousands))_ requests at your Redis server you completely forgot about. It will be nice to get to know them.
+
+Thanks to Salvatore, Redis is blazing fast and you may never notcied those queries existed.
+
+It works like this: a Python script parses the output of the `MONITOR` command. It watches, accumulates, aggregates and reports. In the end it will tell which commands (and how many times) consumed kilowatts on your server.
+
+Example:
+
+```bash
+# reading from stdin
+redis-cli -p 6490 MONITOR | head -n <NUMBER OF LINES TO ANALYZE> | ./redis-faina.py
+```
+
+```bash
+# reading a file
+redis-cli -p 6490 MONITOR | head -n <...> > /tmp/outfile.txt
+./redis-faina.py /tmp/outfile.txt
+```
+
+Keep in mind that running `MONITOR` can reduce the server throughput by more than 50% so you should run `redis-faina` on a `slave` instance.
